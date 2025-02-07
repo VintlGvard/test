55dd81574f0a08b7beda36d80e1204e3fb7f3e1c
VintlGvard <133994035+VintlGvard@users.noreply.github.com>
2025-02-07 12:11:44 +0300

    Update urls.py

--- bank/urls.py
+++ bank/urls.py
@@ -4,6 +4,7 @@ from .views import RegisterUserView, LoginUserView
 auth = [
     path('register/', RegisterUserView.as_view(), name='RegisterClient'),
     path('login/', LoginUserView.as_view(), name='LoginClient'),
+    path('logout/', LogoutUserView.as_view(), name='LogoutClient'),
 ]
 
 urlpatterns = [
